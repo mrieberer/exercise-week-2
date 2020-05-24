787fd4ea327220a5bf46305f27700e65424af533
Michael Rieberer <mrieberer@gmail.com>
2020-05-24 23:12:30 +0200

    Created a .gotignore and test project

---
+++ frontend-test/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+// test suite
+describe('Regression test suite', function(){
+    
+    //test case
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+    })
+
+
+})
