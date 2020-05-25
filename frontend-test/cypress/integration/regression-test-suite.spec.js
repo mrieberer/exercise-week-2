5ba00532375f52e3f969d5a0453e9673344e1411
Michael Rieberer <mrieberer@gmail.com>
2020-05-25 22:32:10 +0200

    created login logout tc

--- frontend-test/cypress/integration/regression-test-suite.spec.js
+++ frontend-test/cypress/integration/regression-test-suite.spec.js
@@ -2,11 +2,18 @@
 
 // test suite
 describe('Regression test suite', function(){
-    
+
     //test case
     it('Perform valid login', function(){
         cy.visit('http://localhost:3000')
+        cy.title().should('include', 'Hotel')
+        cy.contains('Login')
+        cy.get('div.field:nth-child(1) > input:nth-child(2)').type('tester01')
+        cy.get('div.field:nth-child(2) > input:nth-child(2)').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.btn').click()
+        cy.contains('Login')
     })
-
 
 })
